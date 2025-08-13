21161ba0f6cc383f2c15e14dd7dac6ce99b93893
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2025-08-13 12:08:32 +0000

    added category section

--- expense-tracker-with-chart/src/components/TransactionForm.jsx
+++ expense-tracker-with-chart/src/components/TransactionForm.jsx
@@ -3,14 +3,16 @@ import React, { useState } from 'react'
 const TransactionForm = ({onAdd}) => {
 const [ type, setType] = useState("income")
 const [amount,setAmount] = useState("")
-const [desc,setDesc] = useState("")
+const [desc, setDesc] = useState("")
+const [category,setCategory]=useState("")  
 
 const handleSubmit = (e)=>{
    e.preventDefault()
    if(!amount || !desc) return ;
-   onAdd({type , amount:parseFloat(amount) , desc , date:new Date()})
+   onAdd({type , amount:parseFloat(amount) , desc ,category, date:new Date()})
    setAmount("")
-   setDesc("")
+  setDesc("")
+  setCategory("")
 }
 
   return (
@@ -21,7 +23,15 @@ const handleSubmit = (e)=>{
      </select>
 
     <input type="number" value={amount} onChange={(e)=> setAmount(e.target.value)} placeholder='Amount'/>
-    <input type="text" value={desc} onChange={(e)=>setDesc(e.target.value)} placeholder='Description'/>
+      <input type="text" value={desc} onChange={(e) => setDesc(e.target.value)} placeholder='Description' />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Select Category</option>
+        <option value="Salary">Salary</option>
+        <option value="Food">Food</option>
+        <option value="Shopping">Shopping</option>
+        <option value="Transport">Transport</option>
+        <option value="Other">Other</option>
+      </select>
     <button type='submit'> Add</button>
     </form>
   )
